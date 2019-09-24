1eec554b5aaaea6fe67793c6f6fdab7e98ae9c7b
Dori Aviram <50795205+doriaviram-lumigo@users.noreply.github.com>
2019-09-24 09:41:54 +0300

    Nir Cr (#2)

--- src/lumigo_log_shipper/utils/utils.py
+++ src/lumigo_log_shipper/utils/utils.py
@@ -6,6 +6,6 @@ def split_to_chunks(list_to_split: list, chunk_size: int) -> List[list]:
         for i in range(0, len(l), n):
             yield l[i : i + n]  # noqa
 
-    number_of_chunks = max(chunk_size, 1)
-    result = list(chunk(list_to_split, number_of_chunks))
+    chunk_size = max(chunk_size, 1)
+    result = list(chunk(list_to_split, chunk_size))
     return result
