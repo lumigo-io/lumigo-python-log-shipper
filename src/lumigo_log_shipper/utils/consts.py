63e75cf0dd6c0ce74c498a00e3be94837404fbd2
Dori Aviram <dori@lumigo.io>
2019-09-25 18:24:52 +0300

    Fix KILL_SWITCH

--- src/lumigo_log_shipper/utils/consts.py
+++ src/lumigo_log_shipper/utils/consts.py
@@ -9,6 +9,6 @@ ENV = os.environ["ENV"] if IS_LAMBDA_ENVIRONMENT else os.environ["USER"]
 STREAM_NAME = f"{ENV}_logs-edge-stfl_customer-logs-firehose"
 
 TARGET_ACCOUNT_ID = os.environ.get("TARGET_ACCOUNT_ID", "114300393969")
-LOG_STREAM_KIIL_SWITCH = os.environ.get("LOG_STREAM_KIIL_SWITCH")
+LOG_STREAM_KIIL_SWITCH = os.environ.get("LOG_STREAM_KIIL_SWITCH", None) == "TRUE"
 
 SELF_ACCOUNT_ID = "SELF"
