07670ad1b7ca9b060805668195f39b321197935d
Dori Aviram <dori@lumigo.io>
2019-09-25 18:16:39 +0300

    Nir CR

--- src/lumigo_log_shipper/utils/shipper_utils.py
+++ src/lumigo_log_shipper/utils/shipper_utils.py
@@ -8,13 +8,14 @@ def should_report_log(
     function_arn: Optional[str], account_id: str, target_account_id: str, env: str
 ):
     """
-        Protection for streaming logs, we dont send logs of our backend to the same account
+    Protection for streaming logs, we dont send logs of our backend to the same account
     """
     if function_arn:
         func_name = get_function_name_from_arn(function_arn)
-        send_to_myself = (
+        is_sending_to_self = (
             account_id == target_account_id or target_account_id == SELF_ACCOUNT_ID
         )
-        if not send_to_myself or not func_name.startswith(env):
+        is_func_in_my_env = func_name.startswith(env)
+        if not is_sending_to_self or not is_func_in_my_env:
             return True
     return False
