741a8b9a9b14d760b011d175dc180bf2977fde6a
CircleCI <no-reply@build.com>
2019-10-13 14:11:28 +0000

    1.0.8 → 1.0.9. Changes: 9fbb31f Fix role name (#6)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="lumigo-log-shipper",
-    version="1.0.8",
+    version="1.0.9",
     author="Lumigo LTD (https://lumigo.io)",
     author_email="support@lumigo.io",
     description="Ship logs to lumigo platform",
