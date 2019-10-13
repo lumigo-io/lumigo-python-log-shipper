cb89f34d4b58386ae1e353ad498d76bce24da1c7
Dori Aviram <dori@lumigo.io>
2019-10-13 17:09:11 +0300

    Fix role name

--- src/lumigo_log_shipper/utils/sts.py
+++ src/lumigo_log_shipper/utils/sts.py
@@ -1,10 +1,10 @@
 import boto3
 
 
-def assume_role(target_account_id):
+def assume_role(target_account_id, target_env):
     client = boto3.client("sts")
     sts_response = client.assume_role(
-        RoleArn=f"arn:aws:iam::{target_account_id}:role/CustomerLogsWriteRole",
+        RoleArn=f"arn:aws:iam::{target_account_id}:role/{target_env}-CustomerLogsWriteRole",
         RoleSessionName="AssumeCrossAccountRole",
         DurationSeconds=900,
     )
