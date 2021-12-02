67835bd12cffa8e38ed4dfa6327048d02f61ded9
CircleCI <no-reply@build.com>
2021-12-02 15:53:27 +0200

    add exclude filter

--- src/lumigo_log_shipper/utils/shipper_utils.py
+++ src/lumigo_log_shipper/utils/shipper_utils.py
@@ -3,22 +3,34 @@ from typing import List
 from lumigo_log_shipper.models import AwsLogSubscriptionEvent
 
 
-def _is_valid_log(log_message: str, filter_keywords: List[str]):
+def _is_valid_log(
+    log_message: str, filter_keywords: List[str], exclude_filters: List[str] = None
+):
     log_message = str(log_message)
     for keyword in filter_keywords:
+        if exclude_filters:
+            for exclude_filter in exclude_filters:
+                if exclude_filter.lower() in log_message.lower():
+                    return False
+        if keyword.lower() in log_message.lower():
+            return True
         if keyword.lower() in log_message.lower():
             return True
     return False
 
 
 def filter_logs(
-    logs: List[AwsLogSubscriptionEvent], filter_keywords: List[str]
+    logs: List[AwsLogSubscriptionEvent],
+    filter_keywords: List[str],
+    exclude_filters: List[str] = None,
 ) -> List[AwsLogSubscriptionEvent]:
     res_list: List[AwsLogSubscriptionEvent] = []
     for log in logs:
         filtered_events = list(
             filter(
-                lambda event: _is_valid_log(event.message, filter_keywords),
+                lambda event: _is_valid_log(
+                    event.message, filter_keywords, exclude_filters
+                ),
                 log.logEvents,
             )
         )
