ea078835bd9d5d85f8a1665a10d69ed16f84e1f6
CircleCI <no-reply@build.com>
2022-12-14 13:39:17 +0000

    1.0.27 → 1.0.28. Changes: 98b3372 chore: migrate to new workflow (#25)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="lumigo-log-shipper",
-    version="1.0.27",
+    version="1.0.28",
     author="Lumigo LTD (https://lumigo.io)",
     author_email="support@lumigo.io",
     description="Ship logs to Lumigo platform",
